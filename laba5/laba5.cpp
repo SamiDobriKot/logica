340a7828bea5fe02922391a932f575a451823f62
SamiDobriKot <86230447+SamiDobriKot@users.noreply.github.com>
2024-10-23 14:52:22 +0300

    Add files via upload

--- laba5/laba5.cpp
+++ laba5/laba5.cpp
@@ -20,8 +20,16 @@ public:
 		for (int i = 0; i < size; i++) {
 			for (int j = i; j < size; j++) {
 				if (i == j) mass[i][j] = 0;
-				else mass[i][j] = mass[j][i] = rand() % 2;
-				grafsize += mass[i][j];
+				else {
+					grafsize += mass[i][j] =  rand() % 2;
+					if (mass[i][j] == 0) mass[j][i] = 0;
+					if (mass[i][j] == 1) {
+						mass[i][j] -= 2 * (rand() % 2);
+						mass[j][i] = mass[i][j] == 1 ? -1 : 1;
+					}
+				}
+//				else mass[i][j] =  rand() % 2;
+//				grafsize += mass[i][j];
 			}
 		}
 		incid = new char* [size];
@@ -32,7 +40,16 @@ public:
 		for (int i = 0; i < size; i++) {
 			for (int j = i; j < size; j++) {
 				if (mass[i][j] == 0) continue;
-				for (int k = 0; k < size; k++) (k == i) || (k == j) ? incid[k][s] = 1 : incid[k][s] = 0;
+				for (int k = 0; k < size; k++) {//(k == j || k == i) ? incid[k][s] =  mass[i][j]: incid[k][s] = 0;
+					if ((k == j || k == i) && mass[i][j] == 1) {
+						incid[k][s] = -1;
+						mass[i][j] = -1;
+					} else
+						if ((k == j || k == i) && mass[i][j] == -1) {
+						incid[k][s] = 1;
+						mass[i][j] = 1;
+					}else incid[k][s] = 0;
+				}
 				s++;
 			}
 		}
@@ -42,14 +59,15 @@ public:
 		for (int i = 0; i < size; i++) {
 			int count = 0;
 			for (int j = 0; j < size; j++) {
-				if (i == j) std::cout << "# ";
-				else std::cout << (int)mass[i][j] << " ";
+				if (i == j) std::cout << " #";
+//				else std::cout << (int)mass[i][j] << " ";
+				else printf("%2i",mass[i][j]);
 				count += mass[i][j];
 			}
-			std::cout << ":" << count << " ";
-			if (count == 0) std::cout << "Isolated";
-			if (count == 1) std::cout << "Alone";
-			if (count == size - 1) std::cout << "Dominating";
+//			std::cout << ":" << count << " ";
+//			if (count == 0) std::cout << " Isolated";
+//			if (count == 1) std::cout << " Alone";
+//			if (count == size - 1) std::cout << " Dominating";
 
 			std::cout << std::endl;
 		}
@@ -60,13 +78,14 @@ public:
 		for (int i = 0; i < size; i++) {
 			int count = 0;
 			for (int j = 0; j < grafsize; j++) {
-				std::cout << (int)incid[i][j] << " ";
+				//std::cout << (int)incid[i][j] << " ";
+				printf("%2i", incid[i][j]);
 				count += incid[i][j];
 			}
-			std::cout << ":" << count << " ";
-			if (count == 0) std::cout << "Isolated";
-			if (count == 1) std::cout << "Alone";
-			if (count == size - 1) std::cout << "Dominating";
+//			std::cout << ":" << count << " ";
+//			if (count == 0) std::cout << " Isolated";
+//			if (count == 1) std::cout << " Alone";
+//			if (count == size - 1) std::cout << " Dominating";
 			std::cout << std::endl;
 		}
 	}
